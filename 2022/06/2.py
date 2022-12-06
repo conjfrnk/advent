3d58f77687614a25e6a17edabe7254e22b42fb51
Connor Frank <conjfrnk@gmail.com>
2022-12-06 00:36:38 -0500

    postmortem

--- 2022/06/2.py
+++ 2022/06/2.py
@@ -5,27 +5,21 @@ fn = "input.txt"
 fn = Path(Path(__file__).parent, fn)
 dat = open(fn).read().strip().split("\n")
 
-res = 0
-line = dat[0]
-
 
 def check(s):
-    counts = dict()
+    counts = set()
     for c in s:
         if c not in counts:
-            counts[c] = 0
-        counts[c] += 1
-        if counts[c] > 1:
+            counts.add(c)
+        else:
             return False
     return True
 
 
-seen = set()
+line = dat[0]
 req = 14
 
 for i in range(req, len(line)):
     if check(line[i - req : i]):
-        res = i
+        print(i)
         break
-
-print(res)
