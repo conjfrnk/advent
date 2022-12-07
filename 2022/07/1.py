9612a17a34e2c8bb3a5864817b4d399eb4f4592a
Connor Frank <conjfrnk@gmail.com>
2022-12-07 01:19:09 -0500

    2022 day #7

---
+++ 2022/07/1.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from collections import defaultdict
+
+fn = "ex1.txt"
+fn = "input.txt"
+fn = Path(Path(__file__).parent, fn)
+dat = open(fn).read().strip().split("\n")
+
+SZ = defaultdict(int)
+path = []
+for line in dat:
+    words = line.strip().split()
+    if words[1] == "cd":
+        if words[2] == "..":
+            path.pop()
+        else:
+            path.append(words[2])
+    elif words[1] == "ls":
+        continue
+    elif words[0] == "dir":
+        continue
+    else:
+        sz = int(words[0])
+        for i in range(1, len(path) + 1):
+            SZ["/".join(path[:i])] += sz
+
+res = 0
+
+for k, v in SZ.items():
+    if v <= 100000:
+        res += v
+
+print(res)
